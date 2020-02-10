e37e7a6fb1772f50b5316e13c8fff84b24610e1d
rgm93 <rgm93@icloud.com>
2020-02-10 19:17:38 +0100

    v0.1.2.1

---
+++ build/precache-manifest.1c66af7bef9241ba43153740d4e25148.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b41052d8bdf70dd63c616f5b51cedc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fce5368cfbea7d824668",
+    "url": "/static/css/2.d921e62e.chunk.css"
+  },
+  {
+    "revision": "051feb166345e3a5db76",
+    "url": "/static/css/main.02aa9a0f.chunk.css"
+  },
+  {
+    "revision": "fce5368cfbea7d824668",
+    "url": "/static/js/2.500568a5.chunk.js"
+  },
+  {
+    "revision": "a679f83dd0c1ac38894162611631ddd4",
+    "url": "/static/js/2.500568a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "051feb166345e3a5db76",
+    "url": "/static/js/main.78dbe6dc.chunk.js"
+  },
+  {
+    "revision": "e15a4aad777e70aa1d09",
+    "url": "/static/js/runtime-main.0912b4d4.js"
+  },
+  {
+    "revision": "4d2f2f4baa617ba08685b4d9118dc029",
+    "url": "/static/media/bg-pricing.4d2f2f4b.jpeg"
+  },
+  {
+    "revision": "6ee66b2cb8a3d9988e37b83b3b4a590e",
+    "url": "/static/media/card-1.6ee66b2c.jpeg"
+  },
+  {
+    "revision": "ae10829f019b0103525a6cd1102ee213",
+    "url": "/static/media/card-2.ae10829f.jpeg"
+  },
+  {
+    "revision": "4ae4eeff542ab32204e8409d170e14a0",
+    "url": "/static/media/card-3.4ae4eeff.jpeg"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "35f7d126c07a40a893f3f6cc8d1c3717",
+    "url": "/static/media/lock.35f7d126.jpeg"
+  },
+  {
+    "revision": "59fc83e26c663ade5dcae6e7d56ca970",
+    "url": "/static/media/login.59fc83e2.jpeg"
+  },
+  {
+    "revision": "49b54380ba092ba68ab160f3dd8d5773",
+    "url": "/static/media/register.49b54380.jpeg"
+  },
+  {
+    "revision": "f4ad2c7c91543f282a3dbac5592156b0",
+    "url": "/static/media/ruben.f4ad2c7c.jpg"
+  },
+  {
+    "revision": "721ddc3671427ce08a9fa4a0f3f4e8cb",
+    "url": "/static/media/sc1.721ddc36.jpeg"
+  },
+  {
+    "revision": "6d60a64793631851216a98007de81321",
+    "url": "/static/media/sc_original_white.6d60a647.png"
+  },
+  {
+    "revision": "d30c9e30978c9278180dcce2bb66c054",
+    "url": "/static/media/sidebar-2.d30c9e30.jpg"
+  }
+]);
